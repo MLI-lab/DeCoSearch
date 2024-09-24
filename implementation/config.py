9280a6b8718b98b46eab32531b216a7f7f9ec969
franziska <franziska.weindel@tum.de>
2024-09-24 12:28:34 +0000

    Resolved merge conflicts

--- implementation/config.py
+++ implementation/config.py
@@ -26,8 +26,8 @@ class RabbitMQConfig:
       username: Username for authentication with the RabbitMQ server.
       password: Password for authentication with the RabbitMQ server.
     """
-    host: str = 'rabbitmqFW'
-    port: int = 5672
+    host: str = 'localhost'
+    port: int = 5673
     username: str = 'guest'
     password: str = 'guest'
   
@@ -65,8 +65,8 @@ class Config:
   # In this case, default_factory=ProgramsDatabaseConfig means that calling ProgramsDatabaseConfig() (without any arguments) will provide the default value.
   programs_database: ProgramsDatabaseConfig = dataclasses.field(default_factory=ProgramsDatabaseConfig)
   rabbitmq: RabbitMQConfig = dataclasses.field(default_factory=RabbitMQConfig)
-  num_samplers: int = 1
-  num_evaluators: int = 50
+  num_samplers: int = 0
+  num_evaluators: int = 10
   num_pdb: int = 0
   samples_per_prompt: int = 4
   temperature: float = 0.2
